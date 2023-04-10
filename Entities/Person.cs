4ce38868529f4410babbedf9707c55989886276e
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-04-10 17:59:35 +0300

    Реализован класс Person

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,7 +1,16 @@
-﻿namespace PeopleCar.Entities
+﻿using PeopleCar.Geometry;
+
+namespace PeopleCar.Entities
 {
 	public class Person
 	{
-		
+		public string Nickname { get; set; }
+		public Point Coordinates { get; set; }
+
+		public Person(string nickname, Point coordinates)
+		{
+			Nickname = nickname;
+			Coordinates = coordinates;
+		}
 	}
 }
