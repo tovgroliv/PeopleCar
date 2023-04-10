4b38d159fb943fe1be11521903093ee66a40a7cd
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-04-10 18:46:27 +0300

    Добавлен класс Scene
    - При создании формирует место под определнное число сущностей Person и Car

---
+++ Engine/Scene.cs
@@ -0,0 +1,16 @@
+﻿using PeopleCar.Entities;
+
+namespace PeopleCar.Engine
+{
+	public class Scene
+	{
+		public Car[] Cars;
+		public Person[] People;
+
+		public Scene(int carsCount, int peopleCount)
+		{
+			Cars = new Car[carsCount];
+			People = new Person[peopleCount];
+		}
+	}
+}
