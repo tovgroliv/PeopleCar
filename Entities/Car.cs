ac0e1d528146aa4b8520abdb408f4d2b453155f6
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-04-10 18:10:33 +0300

    Реализован класс Car

--- Entities/Car.cs
+++ Entities/Car.cs
@@ -1,7 +1,81 @@
-﻿namespace PeopleCar.Entities
+﻿using PeopleCar.Geometry;
+
+namespace PeopleCar.Entities
 {
 	public class Car
 	{
-		
+		public string Label { get; set; }
+		public Point Coordinates { get; set; }
+		public Person Driver { get; set; }
+
+		public Person[] _passengers = new Person[3];
+
+		public Car(string label, Point coordinates)
+		{
+			Label = label;
+			Coordinates = coordinates;
+		}
+
+		public bool HasPlace
+		{
+			get
+			{
+				for (int i = 0; i < _passengers.Length; i++)
+				{
+					if (_passengers[i] == null)
+					{
+						return true;
+					}
+				}
+
+				return false;
+			}
+		}
+
+		public bool SitDown(Person passenger, int place = -1)
+		{
+			if (place == -1)
+			{
+				for (int i = 0; i < _passengers.Length; i++)
+				{
+					if (_passengers[i] == null)
+					{
+						place = i;
+						break;
+					}
+				}
+			}
+
+			if (_passengers[place] != null)
+			{
+				return false;
+			}
+
+			_passengers[place] = passenger;
+
+			return true;
+		}
+
+		public bool GetUp(Person passenger)
+		{
+			int place = -1;
+			for (int i = 0; i < _passengers.Length; i++)
+			{
+				if (_passengers[i] == passenger)
+				{
+					place = i;
+					break;
+				}
+			}
+
+			if (place == -1)
+			{
+				return false;
+			}
+
+			_passengers[place] = null;
+
+			return true;
+		}
 	}
 }
