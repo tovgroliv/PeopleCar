98747c1fc76aa57beb70efa109c1ec95022db236
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-04-10 17:57:15 +0300

    Реализовн класс Point
    - Координаты 2мерные [x,y]
    - Добавлен метод нахождения расстояния между двумя точками

--- Geometry/Point.cs
+++ Geometry/Point.cs
@@ -4,6 +4,30 @@ namespace PeopleCar.Geometry
 {
 	public class Point
 	{
-		
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public float DistanceTo(Point to)
+		{
+			float distance = Distance(this, to);
+
+			return distance;
+		}
+
+		public static float Distance(Point first, Point second)
+		{
+			float distance = (float)Math.Sqrt(Math.Pow(
+				Math.Abs(first.X - second.X), 2) *
+				Math.Pow(Math.Abs(first.Y - second.Y), 2)
+			);
+
+			return distance;
+		}
 	}
 }
