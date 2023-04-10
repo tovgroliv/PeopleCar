573fc278134afbddf85ceb02f06661b1a079161d
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-04-10 19:02:51 +0300

    Добавлен поиск игроков в радиусе от машины

--- Engine/Scene.cs
+++ Engine/Scene.cs
@@ -1,4 +1,6 @@
 ﻿using PeopleCar.Entities;
+using PeopleCar.Geometry;
+using System.Collections.Generic;
 
 namespace PeopleCar.Engine
 {
@@ -11,6 +13,31 @@ namespace PeopleCar.Engine
 		{
 			Cars = new Car[carsCount];
 			People = new Person[peopleCount];
+		}
+
+		public Person[] FindPeopleAroundCar(Car car, float distance = 15)
+		{
+			List<Person> people = new List<Person>();
+
+			foreach (Car findCar in Cars)
+			{
+				if (car == findCar)
+				{
+					continue;
+				}
+
+				if (findCar.Coordinates.DistanceTo(car.Coordinates) <= distance)
+				{
+					people.Add(findCar.Driver);
+
+					foreach (Person person in findCar.Passengers)
+					{
+						people.Add(person);
+					}
+				}
+			}
+
+			return people.ToArray();
 		}
 	}
 }
